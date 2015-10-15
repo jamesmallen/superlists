d6e875f8fc2d96937548ff46d8eea791a339d80d
James M. Allen <james.m.allen@gmail.com>
2015-10-15 10:39:44 -0400

    passing homepage returns correct html

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,6 +1,8 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
 from .views import home_page
+
 
 # Create your tests here.
 
@@ -10,3 +12,10 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found_homepage = resolve('/')
         self.assertEqual(found_homepage.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
